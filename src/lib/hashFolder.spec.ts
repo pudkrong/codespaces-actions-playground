4df64079c4d2ce667beaa021bad25888db20b35f
Pisut Sritrakulchai <pudkrong@hotmail.com>
2023-04-05 18:24:01 +0200

    feat: add new workflow

--- src/lib/hashFolder.spec.ts
+++ src/lib/hashFolder.spec.ts
@@ -4,7 +4,7 @@ import { hashFolder } from "./hashFolder";
 describe("hashFolder", () => {
   test("should calculate correct MD5 hash value for folder with files", async () => {
     const folderPath = path.join(process.cwd(), "src", "lib", "test-folder");
-    const expectedMd5 = "64c0ce0d8eb5e2073bf63f26f4283aca";
+    const expectedMd5 = "26e971350cee9438a9e89d4b575a7a98";
 
     const actualMd5 = await hashFolder(folderPath);
 
